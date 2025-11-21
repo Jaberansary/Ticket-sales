c973f10cb5189ab980ad268818c9294c53801bfb
Mohshid Jaberansary <Mohshid_Jaberansary@yahoo.com>
2025-11-21 12:40:20 +0330

    add aboutus section.

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,38 @@
+// AboutSectionRight.tsx
+import React from "react";
+
+const AboutUs: React.FC = () => {
+  return (
+    <section className="w-full flex justify-start">
+      <div className="w-full max-w-xl">
+
+        <div className="space-y-5 text-xs leading-relaxed text-black">
+          <div> 
+            <h3 className="mb-1 font-bold text-black text-sm">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+            </h3>
+            <p>
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+              در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
+              نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می‌باشد.
+            </p>
+          </div>
+          <div>
+            <h3 className="mb-1 font-bold text-black text-sm">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+            </h3>
+            <p>
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+              در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
+              نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می‌باشد.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/AbuotUsFlowChart.tsx
@@ -0,0 +1,11 @@
+import FlowChart from "../assets/svg/FlowChart.svg"
+
+
+ export default function AbuotUsFlowChart() {
+  return (
+    <div className="w-full max-w-xl">
+      <img src={FlowChart} alt="چارت تجربه سفر" className="w-full h-auto" />
+    </div>
+  );
+};
+
